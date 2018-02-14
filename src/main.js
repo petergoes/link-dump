c8bf312dd0bfb0c5657d7c76259781a156ebe4bd
Peter Goes <petergoes@gmail.com>
2018-02-14 13:44:49 +0100

    add serviceworker

--- src/main.js
+++ src/main.js
@@ -6,3 +6,15 @@ var app = new App({
 });
 
 export default app;
+
+if ('serviceWorker' in navigator && !/localhost/.test(window.location.href)) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js')
+			.then(registration => {
+				console.log('SW registered: ', registration);
+			})
+			.catch(registrationError => {
+				console.log('SW registration failed: ', registrationError);
+			});
+	});
+}
---
+++ workbox-cli-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"globDirectory": "./public/",
+	"globPatterns": [
+	"**/*.html",
+	"**/*.js",
+	"**/*.css",
+	"**/*.map",
+	],
+	"swDest": "public/sw.js",
+	"clientsClaim": true,
+	"skipWaiting": true
+};
